c58769b38883cbf65a10898db821e988255eecc7
= <ritwick.dsouza@outlook.com>
2016-09-09 15:52:01 +0400

    Made some changes to HTML

--- P2P File Trasfer Application (WebTorrent)/File Seeding/app.js
+++ P2P File Trasfer Application (WebTorrent)/File Seeding/app.js
@@ -9,13 +9,4 @@ function handleFileSelect(ev) {
 function onTorrent (torrent) {
   console.log('Torrent info hash: ' + torrent.infoHash)
   document.getElementById("seed_log").innerHTML = 'Torrent info hash: ' + torrent.infoHash
-  torrent.files.forEach(function (file) {
-    file.createReadStream().pipe(concat(function (buf) {
-      var a = document.createElement('a')
-      a.download = file.name
-      a.href = URL.createObjectURL(new Blob([ buf ]))
-      a.textContent = 'download ' + file.name
-      document.body.appendChild(a)
-    }))
-  })
 }
